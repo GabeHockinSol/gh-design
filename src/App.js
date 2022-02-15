74f2f4ea4628eb74ad76f7629178455d186dd601
gabehockin <38510031+gabehockin@users.noreply.github.com>
2022-02-15 22:24:32 +0000

    Button added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Week1 from "./components/week1/index";
 import Week2 from "./components/week2/index";
+import Week3 from "./components/week3/index";
 
 const Home = () => (
   <nav>
     <h1>Welcome Traveller! </h1>
     <ul>
-      <li>
-        <Link to="/gh-design/week-1"> <h2> Week 1 </h2></Link>
-        <Link to="/gh-design/week-2">Week 2</Link>
-      </li>
+      <li> <Link to="/gh-design/week-1"> <h2> Week 1 </h2></Link> </li>
+      <li> <Link to="/gh-design/week-2"> <h2> Week 2 </h2></Link> </li>
+      <li> <Link to="/gh-design/week-3"> <h2> Week 3 </h2></Link> </li>
     </ul>
   </nav>
 );
@@ -20,6 +20,7 @@ function App() {
     <Routes>
       <Route path='/gh-design/week-1' element={<Week1 />} > </Route>
       <Route path='/gh-design/week-2' element={<Week2 />} > </Route>
+      <Route path='/gh-design/week-3' element={<Week3 />} > </Route>
       <Route path='/gh-design' element={<Home />} > </Route>
     </Routes>
   );
